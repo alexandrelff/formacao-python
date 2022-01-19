01afeca9dad5810ba9b976963952aa18d714219b
alexandrelff <alexandre.fonsecaf@gmail.com>
2022-01-19 17:02:31 -0300

    orientação à objetos 2

--- orientacao_a_objetos_2/modelos.py
+++ orientacao_a_objetos_2/modelos.py
@@ -18,18 +18,17 @@ class Programa:
     @nome.setter
     def nome(self, nome):
         self._nome = nome
-    
-    def __str__(self):
-        return f'{self._nome} - {self.ano} - {self._likes} Likes'
 
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
-            
+
     def __str__(self):
-        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
@@ -37,18 +36,40 @@ class Serie(Programa):
         self.temporadas = temporadas
 
     def __str__(self):
-        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'  
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
+
+
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
 
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 
-filmes_e_series = [vingadores, atlanta]
+lista = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', lista)
 
-for programa in filmes_e_series:
+for programa in minha_playlist:
     print(programa)
+
+print(f'Tamanho: {len(minha_playlist)}')
+print(demolidor in minha_playlist)
